e361f6eeaaadd2d65345eb84079ee3a1f7b71865
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-11-07 09:03:48 -0800

    nds

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from random import randint
+
+for i in range(1, 1200):
+
+    for j in range(0, randint(1, 20)):
+        d = str(i) + ' days ago'
+        with open('file.text', 'a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit --date="' + d + '" -m "commit"')
+
+os.system('git push -u origin main')
