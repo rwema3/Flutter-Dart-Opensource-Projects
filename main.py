428bb6500856e3e521d285713be2fe7edd42d3b4
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-01-01 11:48:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
 
-def makeCommits (days : int):
-    if days < 1:
-        os.system('git push')
     else:
         dates = f"{days} days ago"
         with open('data.txt', 'a') as file:
