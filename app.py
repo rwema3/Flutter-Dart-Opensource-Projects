7c518cf5fc05b51ffdb61777e154f4dde037d30b
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-01-03 12:41:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,3 @@ for i in range(1, 1200):
         with open('file.text', 'a') as file:
             file.write(d)
         os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit"')
-
