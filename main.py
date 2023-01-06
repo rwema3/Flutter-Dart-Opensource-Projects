48d9fa9163e51687522d335285e03c855e6997d5
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-01-06 14:03:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ def makeCommits (days : int):
         os.system('git add data.txt')
 
         # commits
+        os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
+
+        return days * makeCommits(days - 1)
+makeCommits(3)
