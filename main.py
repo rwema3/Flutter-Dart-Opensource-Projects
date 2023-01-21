5c692ac3b3139af758336f69b9527fe226568c57
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-01-21 18:46:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
 import os
 
-def makeCommits (days : int):
-    if days < 1:
-        os.system('git push')
-    else:
-        dates = f"{days} days ago"
-        with open('data.txt', 'a') as file:
-            file.write(f'{dates} <- this was the commit for the !daY!!\n')
-        
-        # staging 
-        os.system('git add data.txt')
-
-        # commits
-        os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
-
-        return days * makeCommits(days - 1)
-makeCommits(3)
