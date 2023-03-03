1e89825650a5ccd1c44913aece77237a0bf5033a
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-03 14:41:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,3 @@ for i in range(1, 1200):
         with open('file.text', 'a') as file:
             file.write(d)
         os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit"')
-
-os.system('git push -u origin main')
