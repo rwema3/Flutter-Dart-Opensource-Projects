fe144a59b78cf0f3853b229cbf24256cab47a5be
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-07 17:32:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from random import randint
 for i in range(1, 1200):
 
     for j in range(0, randint(1, 20)):
-        d = str(i) + ' day ago'
+        d = str(i) + ' days ago'
         with open('file.text', 'a') as file:
             file.write(d)
         os.system('git add .')
