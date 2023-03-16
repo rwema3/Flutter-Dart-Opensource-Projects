1915385c523c388afa33c78567add93404f49b42
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-16 23:34:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def makeCommits (days : int):
         # staging 
         os.system('git add data.txt')
 
-        # c
+        # co
         os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
 
         return days * makeCommits(days - 1)
