0aa62a585ecd2265952ef3f10224770cb5ef4e6e
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-19 00:32:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def makeCommits (days : int):
         with open('data.txt', 'a') as file:
             file.write(f'{dates} <- this was the commit for the !daY!!\n')
         
-        # stagi
+        # stag
         os.system('git add data.txt')
 
         # commiting-
