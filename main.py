97d98cbbceb1841bf3b647cdb1a7f67950251add
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-18 18:33:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,4 @@ def makeCommits (days : int):
         # commiting-
         os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
 
-        return days * makeCommits(days - 1)
     
