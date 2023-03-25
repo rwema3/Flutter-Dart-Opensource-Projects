83b20489d9faf07e8010d0e01172a4b9f993779b
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-03-25 23:59:48 +0200

    b

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ for i in range(1, 1200):
 
     for j in range(0, randint(1, 20)):
         d = str(i) + ' days ago'
-        with open('file.text', 'a') as file:a
+        with open('file.text', 'a') as file:
             file.write(d)
         os.system('git add .')
         os.system('git commit --date="' + d + '" -m "commit"')
