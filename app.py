c3b74305393643d8e1ca8d5ccb34e891f23ef0f2
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-03-27 23:46:57 +0200

    mi

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ for i in range(1, 1200):
         with open('file.text', 'a') as file:
             file.write(d)
         os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "ommit"')
+        os.system('git commit --date="' + d + '" -m "commit"')
 
 os.system('git push -u origin main')
