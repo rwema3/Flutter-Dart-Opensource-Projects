97c7c4a3745150940f4c6d59c63a458da81bb2db
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 19:39:08 +0200

    Pulls

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def makeCommits (days : int):
         os.system('git add data.txt')
 
       
-        os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
+        os.system('git commit --date="'+ dates +'" -m "cupertino library Null safety Flutter widgets implementing the current iOS design language."')
 
         return days * makeCommits(days - 1)
     
