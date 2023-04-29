73b3e314a54fd62f6207b854230c1c7087e07192
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 19:39:38 +0200

    Pulls

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from random import randint
 
-for i in range(1, 3):
+for i in range(1, 1200):
 
-    for j in range(0, randint(1, 9)):
+    for j in range(0, randint(1, 20)):
         d = str(i) + ' days ago'
         with open('file.text', 'a') as file:
             file.write(d)
